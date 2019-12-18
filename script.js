f6e94f13a8343b0bf7f93f636a3ae78cb3472db2
Alicia Burn <AliciaBurn@gmail.com>
2019-12-17 21:04:59 -0500

    Block color coding works

--- script.js
+++ script.js
@@ -1,9 +1,39 @@
 $(document).ready(function(){
+    
     $(".saveBtn").on("click", function() {
         var value = $(this).siblings(".description").val();
+        console.log(value);
         var time = $(this).parent().attr("id");
+        console.log(time);
 
         localStorage.setItem(time, value);
+        
     });
     $("#currentDay").text(moment().format("MMMM Do YYYY"));
 });
+
+
+    var currentHour = moment().format('HH');
+    console.log(currentHour)
+    // loop over time blocks
+    $(".time-block").each(function() {
+      var blockHour = parseInt($(this).attr("id").split("-")[1]);
+    if (blockHour < 9) {
+        blockHour += 12
+    }
+      
+      if (blockHour < currentHour) {
+        $(this).addClass("past");
+      } 
+      else if (blockHour === currentHour) {
+        $(this).removeClass("past");
+        $(this).addClass("present");
+      } 
+      else {
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+        $(this).addClass("future");
+        console.log(currentHour)
+      }
+    });
+// };
